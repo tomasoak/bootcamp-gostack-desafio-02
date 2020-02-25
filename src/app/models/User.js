13a96c0e76e379faa7e724934a3f73cbdc60af62
Tomás <tomas.oak23@gmail.com>
2020-02-25 12:24:52 -0300

    ajustando campos modelo usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -16,7 +16,18 @@ class User extends Model {
       }
     );
 
-      
+      this.addHook('beforeSave', async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt.hash(user.password, 8);
+        }
+      });
 
+      return this;
   }
+
+    checkPassword(password) {
+      return bcrypt.compare(password, this.password_hash);
+    }
 }
+
+export default User;
