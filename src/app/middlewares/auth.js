95b853b1d1a3c26f85d52d8fb193d5a47d030baf
Tomás <tomas.oak23@gmail.com>
2020-03-04 20:52:16 -0300

    add middleware de autenticação

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { promissify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader){
+    return res.status(401).json({ error: 'Token not provided.' })
+  };
+
+  const [, token] = authHeader.split('');
+
+  try {
+    const decoded = await promissify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid.' });
+  }
+  
+  return next();
+}
