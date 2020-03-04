391d560fd897ce51f031d0c32405abbff92c35d1
Tomás <tomas.oak23@gmail.com>
2020-03-04 20:31:20 -0300

    adicionando rotas - controller destinatário

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,14 @@
 import { Router } from 'express';
 
+import RecipientController from './app/controllers/RecipientController';
+
+
 const routes = new Router();
+
+routes.get('/recipients', RecipientController.index);
+routes.get('/recipients/:id', RecipientController.show);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
+routes.delete('recipients/:id', RecipientController.delete);
 
 export default routes; 
